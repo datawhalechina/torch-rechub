0ddd1bf4eed5d50349471a8c32f70f07969f2e31
“morningsky” <sky_lmc@foxmail.com>
2022-05-20 08:26:57 +0800

    add negative_sample and pad_sequence function

--- torch_rechub/basic/utils.py
+++ torch_rechub/basic/utils.py
@@ -179,3 +179,86 @@ def neg_sample(click_hist, item_size):
     while neg in click_hist:
         neg = random.randint(1, item_size)
     return neg
+
+def negative_sample(data, user_col, item_col, label_col, ratio, method_id=2):
+    """Negative Sample method for matching model
+    reference: https://github.com/wangzhegeek/DSSM-Lookalike/blob/master/utils.py
+
+    Args:
+        data (pd.DataFrame): training data
+        user_col (str): user column name
+        item_col (str): item column name for negative sampling
+        label_col (str): label column name
+        ratio (int): negative sample ratio, >= 1
+        method_id (int, optional): `{0 : "random sampling", 1: "sampling method used in word2vec", 2: "tencent RALM sampling"}`. Defaults to 2.
+
+    Returns:
+        pd.DataFrame: (user_id, item_id, label)
+    """
+
+    if not isinstance(ratio, int) or ratio < 1:
+        raise ValueError("ratio means neg/pos, it should be greater than or equal to 1")
+    items_cnt = Counter(data[item_col])
+    items_cnt_order = OrderedDict(sorted((items_cnt.items()), key=lambda x: x[1], reverse=True))
+    user_pos_item = data[data[label_col] == 1].drop(label_col, axis=1).groupby(user_col).agg(list).reset_index()
+    if method_id == 0:
+        def sample(row):
+            neg_items = np.random.choice(list(items_cnt.keys()), size=ratio, replace=False)
+            neg_items = [neg for neg in neg_items if neg not in row[item_col]]
+            return neg_items
+        user_pos_item['neg_'+item_col] = user_pos_item.apply(sample, axis=1)
+    elif method_id == 1:
+        items_cnt_freq = {item: count/len(items_cnt) for item, count in items_cnt_order.items()}
+        p_sel = {item: np.sqrt(1e-5/items_cnt_freq[item]) for item in items_cnt_order}
+        p_value = np.array(list(p_sel.values())) / sum(p_sel.values())
+        def sample(row):
+            neg_items = np.random.choice(list(items_cnt.keys()), size=ratio, replace=False,p=p_value)
+            neg_items = [neg for neg in neg_items if neg not in row[item_col]]
+            return neg_items
+        user_pos_item['neg_'+item_col] = user_pos_item.apply(sample, axis=1)
+    elif method_id == 2:
+        p_sel = {item: (np.log(k + 2) - np.log(k + 1) / np.log(len(items_cnt_order) + 1)) for item, k in
+                 items_cnt_order.items()}
+        p_value = np.array(list(p_sel.values())) / sum(p_sel.values())
+        def sample(row):
+            neg_items = np.random.choice(list(items_cnt.keys()), size=ratio, replace=False, p=p_value)
+            neg_items = [neg for neg in neg_items if neg not in row[item_col]]
+            return neg_items
+        user_pos_item['neg_'+item_col] = user_pos_item.apply(sample, axis=1)
+    else:
+        raise ValueError("method id should in (0,1,2)")
+    neg_data = pd.DataFrame({user_col: user_pos_item[user_col], 'neg_'+item_col: user_pos_item['neg_'+item_col]})
+    neg_data = neg_data.rename(columns={'neg_' + item_col: item_col}, inplace=False)
+    pos_data = pd.DataFrame({user_col: user_pos_item[user_col], item_col: user_pos_item[item_col]})
+    pos_data[label_col] = 1
+    neg_data[label_col] = 0
+    neg_data = neg_data.explode(item_col)
+    pos_data = pos_data.explode(item_col)
+    return pd.concat([pos_data, neg_data])
+
+def pad_sequences(sequences, maxlen=None, dtype='int32',
+                  padding='pre', truncating='pre', value=0.):
+    """ Pads sequences (list of list) to the ndarray of same length.
+        This is an equivalent implementation of tf.keras.preprocessing.sequence.pad_sequences
+        reference: https://github.com/huawei-noah/benchmark/tree/main/FuxiCTR/fuxictr
+    """
+    assert padding in ["pre", "post"], "Invalid padding={}.".format(padding)
+    assert truncating in ["pre", "post"], "Invalid truncating={}.".format(truncating)
+    
+    if maxlen is None:
+        maxlen = max(len(x) for x in sequences)
+    arr = np.full((len(sequences), maxlen), value, dtype=dtype)
+    for idx, x in enumerate(sequences):
+        if len(x) == 0:
+            continue  # empty list
+        if truncating == 'pre':
+            trunc = x[-maxlen:]
+        else:
+            trunc = x[:maxlen]
+        trunc = np.asarray(trunc, dtype=dtype)
+
+        if padding == 'pre':
+            arr[idx, -len(trunc):] = trunc
+        else:
+            arr[idx, :len(trunc)] = trunc
+    return arr
