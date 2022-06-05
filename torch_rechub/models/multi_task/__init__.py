ea8587608f91cf242e2d053374c979a57e62bdef
wk <306178200@qq.com>
2022-06-05 23:19:53 +0800

    gru4rec pr

--- torch_rechub/models/multi_task/__init__.py
+++
@@ -1,5 +0,0 @@
-from .shared_bottom import SharedBottom
-from .esmm import ESMM
-from .mmoe import MMOE
-from .ple import PLE
-from .aitm import AITM
--- torch_rechub/trainers/__init__.py
+++
@@ -1,3 +0,0 @@
-from .ctr_trainer import CTRTrainer
-from .match_trainer import MatchTrainer
-from .mtl_trainer import MTLTrainer
