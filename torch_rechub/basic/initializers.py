28ae09eea62dd7edfecc4fe363331df8721dce89
yinpu <741456392@qq.com>
2022-06-10 20:34:33 +0800

    update the embedding initialization method
    update the embedding initialization method

--- torch_rechub/basic/initializers.py
+++ torch_rechub/basic/initializers.py
@@ -2,7 +2,7 @@ import torch
 
 
 class RandomNormal(object):
-    """Initializer that generates tensors with a normal distribution.
+    """Returns an embedding initialized with a normal distribution.
 
     Args:
         mean (float): the mean of the normal distribution
@@ -13,30 +13,14 @@ class RandomNormal(object):
         self.mean = mean
         self.std = std
 
-    def __call__(self, feature=None, weight=None):
-        """Returns a embedding layer initialized to random normal values or Fill parameter weights with a normal distribution.
-
-        Args:
-            feature (SequenceFeature or SparseFeature, optional): 
-                    If feature is not None, return a embedding layer initialized to random normal values. Defaults to None.
-            weight (torch.nn.parameter.Parameter, optional): 
-                    If weight is not None, fill weight with a normal distribution. Defaults to None.
-
-        Returns:
-            torch.nn.Embedding: If feature is not None.
-            None: If feature is None.
-        """
-        if feature != None:
-            embed = torch.nn.Embedding(feature.vocab_size, feature.embed_dim)
-            weight = embed.weight
-        if weight != None:
-            torch.nn.init.normal_(weight, self.mean, self.std)
-        if feature != None:
-            return embed
+    def __call__(self):
+        embed = torch.nn.Embedding(feature.vocab_size, feature.embed_dim)
+        torch.nn.init.normal_(embed.weight, self.mean, self.std)
+        return embed
 
 
 class RandomUniform(object):
-    """Initializer that generates tensors with a uniform distribution.
+    """Returns an embedding initialized with a uniform distribution.
 
     Args:
         minval (float): Lower bound of the range of random values of the uniform distribution.
@@ -47,21 +31,16 @@ class RandomUniform(object):
         self.minval = minval
         self.maxval = maxval
 
-    def __call__(self, feature=None, weight=None):
-        if feature != None:
-            embed = torch.nn.Embedding(feature.vocab_size, feature.embed_dim)
-            weight = embed.weight
-        if weight != None:
-            torch.nn.init.uniform_(weight, self.minval, self.maxval)
-        if feature != None:
-            return embed
+    def __call__(self):
+        embed = torch.nn.Embedding(feature.vocab_size, feature.embed_dim)
+        torch.nn.init.uniform_(embed.weight, self.minval, self.maxval)
+        return embed
 
 
 class XavierNormal(object):
-    """Initializer that generates tensors according to the method
-    described in `Understanding the difficulty of training deep feedforward
-    neural networks` - Glorot, X. & Bengio, Y. (2010), using a uniform
-    distribution.
+    """Returns an embedding initialized with  the method described in 
+    `Understanding the difficulty of training deep feedforward neural networks` 
+    - Glorot, X. & Bengio, Y. (2010), using a uniform distribution.
 
     Args:
         gain (float): stddev = gain*sqrt(2 / (fan_in + fan_out))
@@ -70,21 +49,16 @@ class XavierNormal(object):
     def __init__(self, gain=1.):
         self.gain = gain
 
-    def __call__(self, feature=None, weight=None):
-        if feature != None:
-            embed = torch.nn.Embedding(feature.vocab_size, feature.embed_dim)
-            weight = embed.weight
-        if weight != None:
-            torch.nn.init.xavier_normal_(weight, self.gain)
-        if feature != None:
-            return embed
+    def __call__(self):
+        embed = torch.nn.Embedding(feature.vocab_size, feature.embed_dim)
+        torch.nn.init.xavier_normal_(embed.weight, self.gain)
+        return embed
 
 
 class XavierUniform(object):
-    """Initializer that generates tensors according to the method
-    described in `Understanding the difficulty of training deep feedforward
-    neural networks` - Glorot, X. & Bengio, Y. (2010), using a uniform
-    distribution.
+    """Returns an embedding initialized with the method described in 
+    `Understanding the difficulty of training deep feedforward neural networks` 
+    - Glorot, X. & Bengio, Y. (2010), using a uniform distribution.
 
     Args:
         gain (float): stddev = gain*sqrt(6 / (fan_in + fan_out))
@@ -93,30 +67,26 @@ class XavierUniform(object):
     def __init__(self, gain=1.):
         self.gain = gain
 
-    def __call__(self, feature=None, weight=None):
-        if feature != None:
-            embed = torch.nn.Embedding(feature.vocab_size, feature.embed_dim)
-            weight = embed.weight
-        if weight != None:
-            torch.nn.init.xavier_uniform_(weight, self.gain)
-        if feature != None:
-            return embed
+    def __call__(self):
+        embed = torch.nn.Embedding(feature.vocab_size, feature.embed_dim)
+        torch.nn.init.xavier_uniform_(embed.weight, self.gain)
+        return embed
 
 
 class Pretrained(object):
     """Creates Embedding instance from given 2-dimensional FloatTensor.
 
     Args:
-        embedding (Tensor): FloatTensor containing weights for the Embedding.
+        embedding_weight(Tensor or ndarray or List[List[int]]): FloatTensor containing weights for the Embedding.
                 First dimension is being passed to Embedding as ``num_embeddings``, second as ``embedding_dim``.
         freeze (boolean, optional): If ``True``, the tensor does not get updated in the learning process.
     """
 
-    def __init__(self, embedding, freeze=True):
-        self.embedding = embedding
+    def __init__(self, embedding_weight, freeze=True):
+        self.embedding_weight = torch.FloatTensor(embedding_weight)
         self.freeze = freeze
 
     def __call__(self, feature):
-        assert feature.vocab_size == self.embedding.shape[0] and feature.embed_dim == self.embedding.shape[1]
-        embed = torch.nn.Embedding.from_pretrained(self.embedding, freeze=self.freeze)
+        assert feature.vocab_size == self.embedding_weight.shape[0] and feature.embed_dim == self.embedding_weight.shape[1]
+        embed = torch.nn.Embedding.from_pretrained(self.embedding_weight, freeze=self.freeze)
         return embed
